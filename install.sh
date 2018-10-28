dc1a09d76759a9f3b0df8511b97b3920c5ed6b31
Flieschi <34345963+Flieschi@users.noreply.github.com>
2018-10-28 14:44:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ function install_sentinel() {
   apt-get -y install python-virtualenv virtualenv >/dev/null 2>&1
   git clone $SENTINEL_REPO ~/.aywacore/sentinel >/dev/null 2>&1
   cd ~/.aywacore/sentinel
-  mv ./sntinel/* .
+  mv ./sentinel/* .
   virtualenv ./venv >/dev/null 2>&1
   ./venv/bin/pip install -r requirements.txt >/dev/null 2>&1
   echo  "* * * * * cd ~/.aywacore/sentinel && ./venv/bin/python bin/sentinel.py >> ~/.aywacore/sentinel.log 2>&1" > ~/.aywacore/aywacore.cron
